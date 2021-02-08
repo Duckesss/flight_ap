313fcdda0d604b27cc93e097b6132477c61a4bc7
Mateus Henrique <mateus.magalhaes@upflow.me>
2021-02-08 02:51:14 -0300

    Método de buscar voo específico

--- src/controllers/VoosController.ts
+++ src/controllers/VoosController.ts
@@ -4,19 +4,36 @@ import Aeroporto from '../controllers/AeroportoController'
 
 class VoosController {
   public async get (req: GetVooRequest, res: Response) : Promise<Response> {
-    const { departure1, departure2 } = req.body
-    let date2
-    if (departure2) {
-      const valid = verifyDateFormat(departure2)
-      !valid && res.status(400).json({ erro: 1, message: 'invalid date' })
-      date2 = new Date(departure2)
-    }
-    const valid = verifyDateFormat(departure1)
-    !valid && res.status(400).json({ erro: 1, message: 'invalid date' })
+    const allString = Object.values(req.query).every(e => typeof e === 'string')
+    if (allString) {
+      console.log(req.query)
+      const { departure1, departure2, passengers } = req.query
+      delete req.query.passengers
+      let date2
+      if (departure2 && typeof departure2 === 'string') {
+        const valid = verifyDateFormat(departure2)
+        if (!valid) { return res.status(400).json({ erro: 1, message: 'invalid date' }) }
+        date2 = new Date(departure2)
+      }
+      if (typeof departure1 === 'string') {
+        const valid = verifyDateFormat(departure1)
+        if (!valid) { return res.status(400).json({ erro: 1, message: 'invalid date' }) }
 
-    const date1 = new Date(departure1)
-    const voos = await Voo.find({ ...req.body, departure1: date1, departure2: date2 })
-    return res.json(voos)
+        if (typeof req.query.origin === 'string' && typeof req.query.destination === 'string') {
+          req.query.origin = await Aeroporto.getAeroporto(req.query.origin)
+          req.query.destination = await Aeroporto.getAeroporto(req.query.destination)
+          const date1 = new Date(departure1)
+          const voos = await Voo.find({ ...req.query, departure1: date1, departure2: date2 })
+          const allowedFlights = voos.filter(voo => voo.totalPassengers >= (Number(passengers) + voo.passengers))
+          return res.json(allowedFlights)
+        }
+      }
+    } else {
+      return res.status(400).json({
+        erro: 1,
+        message: 'Parâmetros enviados incorretamente'
+      })
+    }
   }
 
   public async getAll (req: Request, res: Response) : Promise<Response> {
